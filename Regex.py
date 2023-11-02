c9c3841ac19ebb917f105aded800ca19a3b9a3af
chrnz9721 <christophernziengui9721@gmail.com>
2023-11-02 15:39:08 +0100

    mise a jour 2 du fichier Regex.py

--- Regex.py
+++ Regex.py
@@ -1,9 +1,4 @@
 import re
 
-
-
-text = 'imei:864895031562505,tracker,231031121212,F,121212.00,A,0022.97401,N,00927.26038,E,'
-regex = r'imei:(\d+),(\w+),(\d{6})(\d{6}),(.),(\d{6}.\d{2}),(.),(\d{4}.\d{5}),(.),(\d{5}.\d{5}),(.)'
-match = re.search(regex, text)
-data = { 'imei': match.group(1), 'type': match.group(2), 'date': match.group(3), 'time': match.group(4), 'status': match.group(5), 'latitude': match.group(7) + ''  + match.group(8), 'longitude': match.group(9) + '' + match.group(10) }
-print(data)
+m = re.match(r'imei:(?P<imei>\d+),(?P<type>\w+),(?P<date>\d{6})(?P<time>\d{6}),(?P<statut>.),(?P<heure>\d{6}.{3}),\w,(?P<latitude>\d{4}.{6}),(?P<lat>\w),(?P<longitude>\d{5}.{6}),(?P<long>\w)','imei:864895031562505,tracker,231031121212,F,121212.00,A,0022.97401,N,00927.26038,E,')
+print(m.groupdict())
