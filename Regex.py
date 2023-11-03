c9a900dc9f84b96c5406d1a2a9144136889d031f
chrnz9721 <christophernziengui9721@gmail.com>
2023-11-03 11:58:44 +0100

    indication coté et ajout de signe a la latitude et longitude modificaton de la fonction de calcule de coordonnées.

--- Regex.py
+++ Regex.py
@@ -1,26 +1,36 @@
 import re
 
-m = re.match(r'imei:(?P<imei>\d+),(?P<type>\w+),(?P<date>\d{6})(?P<time>\d{6}),(?P<statut>.),(?P<heure>\d{6}.{3}),\w,(?P<latitude>\d{4}.{6}),(?P<lat>\w),(?P<longitude>\d{5}.{6}),(?P<long>\w)','imei:864895031562505,tracker,231031121212,F,121212.00,A,0022.97401,N,00927.26038,E,')
+m = re.match(r'imei:(?P<imei>\d+),(?P<type>\w+),(?P<date>\d{6})(?P<time>\d{6}),(?P<statut>.),(?P<heure>\d{6}.{3}),\w,(?P<latitude>\d{4}.{6}),(?P<laty>\w),(?P<longitude>\d{5}.{6}),(?P<longy>\w)','imei:864895031562505,tracker,231031121212,F,121212.00,A,0022.97401,S,00927.26038,E,')
 print(m.groupdict())
 
 
-def calculer_coordonnees(m):
-    def calculer(coordonnee):
-        loc = re.search(r'^\d{2,3}', m.group(coordonnee))
-        val = re.search(r'\d{2}.\d{5}', m.group(coordonnee))
+def calculer_latitude(m):
+    loc = re.search(r'^\d{2}', m.group("latitude"))
+    lat = re.search(r'\d{2}.\d{5}', m.group("latitude"))
 
-        loc = int(loc.group())
-        val = float(val.group())
+    loc = int(loc.group())
+    lat = float(lat.group())
 
-        val = val / 60
-        result = loc + val
+    lat = lat / 60
+    ind = m.group("laty")
+    result = loc + lat
+    if ind == "N":
+        print(f'la latitude est de : {result} Nord')
+    else:
+        print(f'la latitude est de : {-result} Sud')
+def calculer_longitude(m):
+    loc = re.search(r'^\d{3}', m.group("longitude"))
+    lon = re.search(r'\d{2}.\d{5}', m.group("longitude"))
 
-        return result
+    loc = int(loc.group())
+    lon = float(lon.group())
 
-    latitude = calculer("latitude")
-    longitude = calculer("longitude")
-
-    print(f'la latitude est de : {latitude}')
-    print(f'la longitude est de : {longitude}')
-
-calculer_coordonnees(m)
+    lon = lon / 60
+    result = loc + lon
+    ind = m.group("longy")
+    if ind == "E":
+        print(f'la latitude est de : {result} Est')
+    else:
+        print(f'la latitude est de : {-result} Ouest')
+calculer_longitude(m)
+calculer_latitude(m)
